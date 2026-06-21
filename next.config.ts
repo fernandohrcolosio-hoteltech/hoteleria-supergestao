aed9f14b2bcf56d6c000d905ec6ada1604474c9b
Fernando Colosio <fernando.colosio@jfl.inc>
2026-06-21 00:35:26 -0300

    revert: remove invalid cspNonce config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    cspNonce: false,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
